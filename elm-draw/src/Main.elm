a08a9d7746c5efed928d598b91c8310ce9a740d5
Masahiro Atsumi <raindro.aqua@gmail.com>
2019-08-13 18:41:56 +0900

    #1, #4 (https://scrapbox.io/Lipton-Elm/ElmSVG%E3%81%8A%E5%8B%89%E5%BC%B7%E8%A8%98%E9%8C%B2%E7%9B%AE%E6%AC%A1%E3%83%9A%E3%83%BC%E3%82%B8)

--- elm-draw/src/Main.elm
+++ elm-draw/src/Main.elm
@@ -1,5 +1,79 @@
-module Main exposing (main)
+module Main exposing (Model, Msg(..), init, update, view)
 
-import Html exposing (text)
+import Browser
+import Color
+import Html exposing (Html, div, p, text)
+import Html.Attributes
+import Html.Events.Extra.Mouse as Mouse
+import TypedSvg exposing (rect, svg)
+import TypedSvg.Attributes exposing (stroke, viewBox)
+import TypedSvg.Attributes.InPx exposing (height, width, x, y)
 
-main = text "hi"
+
+main =
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { mousepos : ( Float, Float )
+    }
+
+
+init : Model
+init =
+    { mousepos = ( 0, 0 ) }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = VBoxMove ( Float, Float )
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        VBoxMove ( x, y ) ->
+            { model | mousepos = ( x, y ) }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ p [] [ text <| Debug.toString model.mousepos ]
+        , svgArea model
+        ]
+
+
+svgArea model =
+    svg
+        [ Html.Attributes.style "background-color" "ghostwhite"
+        , Html.Attributes.style "border" "1px solid black"
+        , width 500
+        , height 500
+        , viewBox 0 0 500 500
+        , Mouse.onMove (\event -> VBoxMove event.offsetPos)
+        ]
+        [ rect
+            [ stroke <| Color.black
+            , height 50
+            , width 50
+            , x <| Tuple.first model.mousepos
+            , y <| Tuple.second model.mousepos
+            ]
+            []
+        ]
